6a08d52eebbf4dac550d6cfb286e3b9bd0564b63
Michael Morello <michael.morello@gmail.com>
2015-03-11 14:06:22 +0100

    Check if enrollment is ok before creating flag + add a warning in playbook

--- roles/ipa-client/templates/join.sh
+++ roles/ipa-client/templates/join.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ipa-client-install --force-join -U --domain={{ipa_domain}} --server={{ipa_server}} -p {{ipa_admin}} --ntp-server={{ntp_server}} -w {{ipa_password}} >> /var/log/ipa-client-install.log
-touch /var/ipa.ok
+ipa-client-install --force-join -U --domain={{ipa_domain}} --server={{ipa_server}} -p {{ipa_admin}} --ntp-server={{ntp_server}} -w {{ipa_password}}
+if [ $? -eq 0 ]; then
+    touch /var/ipa.ok
+fi
 
 rm -- "$0"
